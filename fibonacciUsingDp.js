a2259d60e2978e8566dade8edc05c7922bf245d8
tu <tu@s20s.net>
2024-04-29 12:54:56 +0530

    fib num using bottom up approch

--- fibonacciUsingDp.js
+++ fibonacciUsingDp.js
@@ -12,9 +12,24 @@ const fib = (n) => {
   return fn(n);
 };
 
+const fibUsingLoop = (n) => {
+  if (n <= 1) return n;
+  let a = 0,
+    b = 1,
+    c = 0;
+  for (let i = 1; i < n; i++) {
+    c = a + b;
+    a = b;
+    b = c;
+  }
+  return c;
+};
+
 console.log(fib(2));
 console.log(fib(3));
 console.log(fib(5));
 console.log(fib(7));
 console.log(fib(30));
-console.log(fib(100));
+console.log(fibUsingLoop(100));
+console.log(fibUsingLoop(0));
+console.log(fibUsingLoop(1));
